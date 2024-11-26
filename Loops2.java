f05b0fbf862b1d91f1ccd1bcaf5ef44a425907bf
mohamedali0077 <mohxali77@gmail.com>
2024-11-26 12:55:49 +0530

    Add files via upload
    day3(ex:3)

---
+++ Loops2.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Loops2 {
+	public static void main(String []args) {
+//		for(int num=1;num<=10;num++) {
+//			System.out.println("name");
+//		}
+		for (int i=1;i<=10;i++) {
+			System.out.println("5x"+i+"="+i*5);
+		}
+		
+	}
+
+}
