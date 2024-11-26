123778f7e52fa03fef4c04ffb367e6c261731f59
mohamedali0077 <mohxali77@gmail.com>
2024-11-26 13:12:28 +0530

    Add files via upload
    day3

--- Loops2.java
+++ Loops2.java
@@ -1,12 +1,16 @@
 package day3;
-
+import java.util.Scanner;
 public class Loops2 {
 	public static void main(String []args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the table");
+		int table=scan.nextInt();
+		
 //		for(int num=1;num<=10;num++) {
 //			System.out.println("name");
 //		}
 		for (int i=1;i<=10;i++) {
-			System.out.println("5x"+i+"="+i*5);
+			System.out.println(table+"x"+i+"="+i*table);
 		}
 		
 	}
