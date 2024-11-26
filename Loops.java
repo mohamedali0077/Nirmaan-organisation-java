7656246f5d20b3990d483558c5c3ccb28cba14ec
mohamedali0077 <mohxali77@gmail.com>
2024-11-26 12:13:17 +0530

    Add files via upload
    day3

---
+++ Loops.java
@@ -0,0 +1,31 @@
+package day3;
+import java.util.Scanner;
+public class Loops {
+	
+	public static void main(String[]args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter your marks");
+		
+		int mark=scan.nextInt();
+	if (mark>=35);{
+	System.out.println("Grade D");
+		if(mark>=90) {
+			System.out.println("Grade A");}
+			else if(mark>=75) {
+				System.out.println("Grade B");
+				
+				
+			}else if(mark>=50) {
+				System.out.println("Grade C");
+			}
+		}
+			
+			}
+		
+	}
+	
+		
+	
+
+
+
