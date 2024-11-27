ab9245688e0373c52b86d3932a2018464045f362
mohamedali0077 <mohxali77@gmail.com>
2024-11-27 13:04:30 +0530

    Add files via upload
    day4

---
+++ Name.java
@@ -0,0 +1,14 @@
+package Day4;
+
+public class Name {
+	public static void main(String []args) {
+		for(int i=1;i<=100;i++) {
+			if(i%3==0){
+				System.out.println("mohamed");
+			}else {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
