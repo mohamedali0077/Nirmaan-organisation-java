a9ca2d4c692898747add5e8fff87bf8ae37901bb
mohamedali0077 <mohxali77@gmail.com>
2024-11-28 12:28:09 +0530

    Add files via upload
    day5

---
+++ StringConcept.java
@@ -0,0 +1,18 @@
+package day5;
+import java.util.Scanner;
+public class StringConcept {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter your Name");
+		
+		String name="Mohamed Ali";
+		System.out.println("name");
+		System.out.println(name.length());
+		System.out.println(name.substring(0,7));
+		System.out.println(name.charAt(0));
+		
+		
+		
+	}
+
+}
