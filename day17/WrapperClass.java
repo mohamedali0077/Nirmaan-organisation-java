7b13e77ff0e8c1dbf9ae557dc6c33975a6eb606d
mohamedali0077 <mohxali77@gmail.com>
2024-12-17 13:12:11 +0530

    Add files via upload
    day17

---
+++ day17/WrapperClass.java
@@ -0,0 +1,69 @@
+package day17;
+
+public class WrapperClass{
+
+	
+		           public static void main(String[] args) {
+					
+		        	   int num=15;
+		        	   byte num1=20;
+		        	   boolean value=true;
+		        	   char letters=('d');
+		        	   double num2=1.00;
+		        	   float num3=4.0f;
+		        	   short num4=125;
+		        	   
+		        	   
+		        	   Integer autoBoxingInt=num;
+		        	   System.out.println(autoBoxingInt);
+		        	   int unboxingInt=num;
+		        	   System.out.println(unboxingInt);
+		        	   
+		        	   Byte autoBoxingByte=num1;
+		        	   System.out.println(autoBoxingByte);
+		        	   byte unboxingByte=num1;
+		        	   System.out.println(unboxingByte);
+		        	   
+		        	   
+		        	   
+		        	   Boolean autoBoxingBoolean=value;
+		        	   System.out.println(autoBoxingBoolean);
+		        	   boolean unboxingboolean=value;
+		        	   System.out.println(unboxingboolean);
+		        	   
+		        	   Character autoBoxingCharacter=letters;
+		        	   System.out.println(autoBoxingCharacter);
+		        	   char unboxingchar=letters;
+		        	   System.out.println( unboxingchar);
+		        	   
+		        	   Double autoBoxingDouble=num2;
+		        	   System.out.println(autoBoxingDouble);
+		        	   double unboxingdouble=num2;
+		        	   System.out.println(unboxingdouble);
+		        	   
+		        	   Float autoBoxingFloat=num3;
+		        	   System.out.println(autoBoxingFloat);
+		        	   float unboxingfloat=num3;
+		        	   System.out.println(unboxingfloat);
+		        	   
+		        	   Short autoBoxingShort=num4;
+		        	   System.out.println(autoBoxingShort);
+		        	   short unboxingshort=num4;
+		        	   System.out.println( unboxingshort);
+		        	   
+		        	   
+		        	   
+		        	   
+				}                                                     
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+ 
