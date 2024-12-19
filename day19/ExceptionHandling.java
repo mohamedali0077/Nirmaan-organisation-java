ad12b214b10fae973bfd186baa827cf8777cabf6
mohamedali0077 <mohxali77@gmail.com>
2024-12-19 11:52:41 +0530

    Add files via upload
    day 19

---
+++ day19/ExceptionHandling.java
@@ -0,0 +1,60 @@
+package day19;
+
+import java.util.Scanner;
+
+public class ExceptionHandling {
+
+	              public static void main(String[] args) {
+					Scanner sc=new Scanner(System.in);
+					
+					try {
+						System.out.println("Enter your age");
+						int age=sc.nextInt();
+						
+						if(age>0) {
+							System.out.println(age);
+						}
+					else {
+						throw new ArithmeticException();
+						
+					}}
+					catch(ArithmeticException age) {
+					System.out.println("enter correct age");
+					}
+					catch(Exception E) {
+						System.out.println(E);
+						
+					}
+					finally {
+						System.out.println("age is not accept");
+					}
+					
+					
+					
+					
+					
+					
+					}
+				}
+
+				
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
